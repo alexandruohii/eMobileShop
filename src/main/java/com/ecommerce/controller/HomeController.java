b68e2b3bc678e736f1365b9e6833e5cc922d9ea0
alexandruohii <alexandru.ohii@gmail.com>
2016-08-20 22:29:48 +0300

    Add

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
     }
 
     @RequestMapping("/accessDenied")
-    public String about() {
+    public String accessDenied() {
         return "accessDenied";
     }
 
